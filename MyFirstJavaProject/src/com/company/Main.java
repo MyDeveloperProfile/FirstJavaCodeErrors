ad4c508e1ab2252a558db8b0e3c8f8a5d55fb1e9
Nick Vinson <69322383+MyDeveloperProfile@users.noreply.github.com>
2021-01-07 14:13:00 -0600

    First Commit
    Errors

---
+++ MyFirstJavaProject/src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	      System.out.println("Hello Java")  //error 1 for line 8. Expected ; at the end of the line
+
+//        System.outprint("Hi"); // This was because there was not a . between out and print
+        /*java: cannot find symbol
+        symbol:   method outprint(java.lang.String)
+                location: class java.lang.System*/
+
+
+
+
+	//Scanner input = new ScannerSystem.in);
+        // java: '(' or '[' expected
+
+
+        //System.out.println('what is your name?');
+        //java: unclosed character literal
+
+//        String firstName = input.nextLine[];
+//        java: '.class' expected
+
+
+//        System.out.printg("Hello %s!\n", firstName);
+//        java: cannot find symbol
+//        symbol:   variable firstName
+//        location: class com.company.Main
+
+
+
+//        Sysem.out.println("What is your last name");
+//        java: package Sysem does not exist
+
+
+
+
+    }
+}
